13941ef78e8a0a460665a88f9d3751bd75eca93f
mizzdawg <mcarty79@gmail.com>
2025-12-09 19:01:09 -0500

    Adding attendule module to game details page with new styling

--- demoONLY.js
+++ demoONLY.js
@@ -2,6 +2,7 @@ const availBtn = document.querySelector('.avail-btn');
 const availModule = document.querySelector('.avail-module');
 const availModileX = document.querySelector('.avail-module-x');
 const availFormBtn = document.querySelector('.avail-module-submit');
+const gameDetailAvailBtn = document.querySelector('.btn-edit-table')
 
 
 availBtn.addEventListener('click', () => {
@@ -13,6 +14,10 @@ availModileX.addEventListener('click', () => {
 })
 
 availFormBtn.addEventListener('click', () => {
+    toggleAvailModule();
+})
+
+gameDetailAvailBtn.addEventListener('click', () => {
     toggleAvailModule();
 })
 
